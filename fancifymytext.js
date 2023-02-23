234ff9127e87b12a873dccc15c62d9a8eb9a5038
pprabhu2727 <prasadprabhu@hotmail.com>
2023-02-23 12:14:47 -0800

    Edited to pass linting/validation

--- fancifymytext.js
+++ fancifymytext.js
@@ -1,36 +1,33 @@
-function sendAlert(){
-    alert("Hello, world!");
+function sendAlert () {
+  alert('Hello, world!')
 }
 
-function makeBig(){
-    //2em is equivalent to 24pt font
-    let t = document.getElementById("txtArea");
-    t.style.fontSize = "2em";
+function makeBig () {
+  // 2em is equivalent to 24pt font
+  const t = document.getElementById('txtArea')
+  t.style.fontSize = '2em'
 }
 
-function makeFancy(){
-    let t = document.getElementById("txtArea");
-    t.style.fontWeight = "bold";
-    t.style.color = "blue";
-    t.style.textDecoration = "underline";
+function makeFancy () {
+  const t = document.getElementById('txtArea')
+  t.style.fontWeight = 'bold'
+  t.style.color = 'blue'
+  t.style.textDecoration = 'underline'
 }
 
-function makeBoring(){
-    let t = document.getElementById("txtArea");
-    t.style.fontWeight = "normal";
-    t.style.color = "black";
-    t.style.textDecoration = "none";
+function makeBoring () {
+  const t = document.getElementById('txtArea')
+  t.style.fontWeight = 'normal'
+  t.style.color = 'black'
+  t.style.textDecoration = 'none'
 }
 
-function doMoo(){
-    let t = document.getElementById("txtArea");
-    t.style.textTransform = "uppercase";
-    let sentenceArray = t.value.split(".");
-    for (let i = 0; i<sentenceArray.length - 1; i++){
-        sentenceArray[i] = sentenceArray[i] + "-Moo";
-    }
-    t.value = sentenceArray.join(".");
-
+function doMoo () {
+  const t = document.getElementById('txtArea')
+  t.style.textTransform = 'uppercase'
+  const sentenceArray = t.value.split('.')
+  for (let i = 0; i < sentenceArray.length - 1; i++) {
+    sentenceArray[i] = sentenceArray[i] + '-Moo'
+  }
+  t.value = sentenceArray.join('.')
 }
-
-
